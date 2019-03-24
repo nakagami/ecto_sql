049d66ebeb2246de3ef695e3a40a0b2b30b9f4f9
Hajime Nakagami <nakagami@gmail.com>
2019-03-24 11:18:26 +0900

    comment out IO.puts

--- lib/ecto/adapters/postgres/connection.ex
+++ lib/ecto/adapters/postgres/connection.ex
@@ -72,7 +72,7 @@ if Code.ensure_loaded?(Postgrex) do
 
     @impl true
     def query(conn, sql, params, opts) do
-      IO.puts sql
+      #IO.puts sql
       Postgrex.query(conn, sql, params, opts)
     end
 
